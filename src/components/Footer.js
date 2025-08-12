98a755a91a92272a97c27aab58c960902c61db58
Harr24 <137282048+Harr24@users.noreply.github.com>
2025-08-13 00:41:44 +0700

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white p-4 mt-20">
       <div className="container mx-auto text-center">
         <p> & copy; 2024 Yusuf Harry Setiawan. </p>{" "}
-        <div className="mt-2">
+        <div className="mt-2 flex justify-center space-x-4">
           <a
             href="https://github.com/Harr24"
             target="_blank"
@@ -13,6 +14,22 @@ const Footer = () => {
             className="text-blue-400 hover:underline"
           >
             GitHub{" "}
+          </a>{" "}
+          <a
+            href="https://www.linkedin.com/in/yusuf-harry-ab8a2a284"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-400 hover:underline"
+          >
+            LinkedIn{" "}
+          </a>{" "}
+          <a
+            href="https://www.instagram.com/harrfly_/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-400 hover:underline"
+          >
+            Instagram{" "}
           </a>{" "}
         </div>{" "}
       </div>{" "}
